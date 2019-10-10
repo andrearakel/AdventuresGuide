1e59047e3af8cc83b44684050aeebecf0cf612bb
Andrea Rakel <andrearakel1@gmail.com>
2019-10-10 10:15:11 +0000

    grunnur

---
+++ src/main/java/is/hi/hbv501g/adventuresguide/Adventure/s/Guide/Application.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.adventuresguide.Adventure.s.Guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
